0600ac9a1708af4500f254efed49f1c5f6cebe4d
DevendarReddy21 <deva.9695@gmail.com>
2021-06-06 16:11:17 +0000

    pull.sh latest commit

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/
-sudo chown -R centos:centos script/
+sudo chown -R centos:centos script
 cd script/webapp
 export PATH=/opt/maven/apache-maven-3.8.1/bin:$PATH
 mvn clean package

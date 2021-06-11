a06a757e36f1189ca2a113c6544fe20694cd5820
DevendarReddy21 <deva.9695@gmail.com>
2021-06-11 17:23:46 +0000

    push

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /opt/webapp/target
-cp *.war centos@172.31.23.88:/opt/apache-tomcat-8.5.42/webapps
+cd /opt/script/webapp/target
+scp *.war centos@172.31.23.88:/opt/apache-tomcat-8.5.42/webapps
+
+
